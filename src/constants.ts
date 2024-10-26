6794b0b86dbf25bb758b630ec1a75ff4d7eed8c2
deniel_maertukaev <khhkhhkkh@gmail.com>
2024-10-26 05:31:22 +0300

    added dragon harvest buff short name

--- src/constants.ts
+++ src/constants.ts
@@ -4,6 +4,7 @@ export const ICON_SIZE = 20
 export const BUFF_NAMES: Record<string, string> = {
   "frenzy": "F",
   "click_frenzy": "CF",
+  "dragon_harvest": "DH",
   "dragonflight": "DF",
   "cursed_finger": "CuF",
   "cookie_storm": "CS",
